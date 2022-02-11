c9d267bbaf6df57577e55699fccc989484df15d1
Corina <k.gorodetskaya@icloud.com>
2022-02-11 07:12:37 +0000

    Strapi Conf

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Index({ allPosts, preview }) {
     <>
       <Layout preview={preview}>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Hello to Life Of Fyodor Blog</title>
         </Head>
         <Container>
           <Intro />
